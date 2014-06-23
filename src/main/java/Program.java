0d8c73a38675a1e2b8a3d5833cb5d55e7d617c22
ati-ozgur <ati.ozgur@gmail.com>
2014-06-23 10:14:26 +0300

    Java code exec

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,7 +2,7 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Merhaba, KDD99");
-        //weka.core.SystemInfo.main(args);
+        weka.core.SystemInfo.main(args);
     }
 
 }
