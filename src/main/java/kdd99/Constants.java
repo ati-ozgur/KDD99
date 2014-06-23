e9d52db46b9d7f0e0d496357ab83fae6eb10ac91
ati-ozgur <ati.ozgur@gmail.com>
2014-06-23 11:19:35 +0300

    refactor

---
+++ src/main/java/kdd99/Constants.java
@@ -0,0 +1,7 @@
+package kdd99;
+
+public class Constants
+{
+    public static final String DATABASE_URL = "Datasets/test.db";
+    public static final String JDBC_URL = "jdbc:sqlite:" + DATABASE_URL;
+}
--- src/main/java/kdd99/SQLiteJDBC.java
+++ src/main/java/kdd99/SQLiteJDBC.java
@@ -10,7 +10,7 @@ public class SQLiteJDBC
     Connection c = null;
     try {
       Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      c = DriverManager.getConnection(Constants.JDBC_URL);
     } catch ( Exception e ) {
       System.err.println( e.getClass().getName() + ": " + e.getMessage() );
       System.exit(0);
