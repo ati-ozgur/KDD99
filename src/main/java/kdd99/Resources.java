ec5521360332ff545f9377721813a3ab7c7b099a
ati-ozgur <ati.ozgur@gmail.com>
2014-07-01 18:48:32 +0300

    rename package line

--- src/main/java/kdd99/Resources.java
+++
@@ -1,9 +0,0 @@
-package kdd99;
-
-public class Resources
-{
-    public static final String SQL_kdd99TableCreation = Constants.RESOURCES + "kdd99TableCreation.sql";
-    public static final String SQL_Import = Constants.RESOURCES + "import.sql";
-
-
-}
