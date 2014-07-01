9c1fe66e847aadf4a55a5e60c53badcfc7f51350
ati-ozgur <ati.ozgur@gmail.com>
2014-07-01 18:47:44 +0300

    move package

---
+++ src/main/java/tr/edu/baskent/fbe/Kdd99/Resources.java
@@ -0,0 +1,9 @@
+package kdd99;
+
+public class Resources
+{
+    public static final String SQL_kdd99TableCreation = Constants.RESOURCES + "kdd99TableCreation.sql";
+    public static final String SQL_Import = Constants.RESOURCES + "import.sql";
+
+
+}
