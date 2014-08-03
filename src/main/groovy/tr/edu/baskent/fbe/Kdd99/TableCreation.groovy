f251430ddca1779d30d942dd7140fbb02cb4613f
Atilla Ozgur <ati.ozgur@gmail.com>
2014-08-03 21:01:13 +0300

    table creation finished

--- src/main/groovy/tr/edu/baskent/fbe/Kdd99/TableCreation.groovy
+++ src/main/groovy/tr/edu/baskent/fbe/Kdd99/TableCreation.groovy
@@ -15,13 +15,17 @@ def sqlEngine = Sql.newInstance(Constants.JDBC_URL)
 
 String sqlCreateTable = new File(Resources.SQL_kdd99TableCreation).text
 
-println sqlCreateTable
-
+// println sqlCreateTable
+// sqlEngine.execute.sqlCreateTable
 String[] sqlCreateTableList = sqlCreateTable.split(";")
 
 for(String sql in sqlCreateTableList) {
-    println "---------------------"
-    sqlEngine.execute sql
+    boolean containsData = sql?.trim()
+    if (containsData)
+    {
+        sqlEngine.execute sql
+        println "--------------------- Table Creation Sql Executed ---------------------"
+    }
 
 }
 
