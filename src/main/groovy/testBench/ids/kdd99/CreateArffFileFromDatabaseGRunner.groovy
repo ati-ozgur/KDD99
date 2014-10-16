6755ba25e4f92ef2592179640c76894e9cfba081
ati-ozgur <ati.ozgur@gmail.com>
2014-10-16 16:17:34 +0300

    Create arff from sql file v1

---
+++ src/main/groovy/testBench/ids/kdd99/CreateArffFileFromDatabaseGRunner.groovy
@@ -0,0 +1,13 @@
+package testBench.ids.kdd99
+
+
+println "Hello from groovy"
+
+//weka.core.SystemInfo.main(null);
+
+String sql = new File(Resources.SQL_MAIN_FOLDER + "createFullKdd99DatasetLimit10.sql").text
+
+
+println sql
+
+// ArffFileFromDatabase m = new ArffFileFromDatabase();
--- src/main/java/testBench/ids/kdd99/Resources.java
+++ src/main/java/testBench/ids/kdd99/Resources.java
@@ -2,6 +2,8 @@ package testBench.ids.kdd99;
 
 public class Resources
 {
+
+    public static final String SQL_MAIN_FOLDER=Finals.RESOURCES;
     public static final String SQL_kdd99TableCreation = Finals.RESOURCES + "kdd99TableCreation.sql";
     public static final String SQL_Import = Finals.RESOURCES + "import.sql";
 
