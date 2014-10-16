87d5ea046a4a962a969e01b99ce46595d717c2c5
ati-ozgur <ati.ozgur@gmail.com>
2014-10-16 15:55:00 +0300

    control statement for sql

---
+++ src/main/resources/ControlSqlStatements.sql
@@ -0,0 +1,12 @@
+-- Control Sql Statements
+-- in the main folder of project run following commands to open sqlite database
+
+
+sqlite3.exe Datasets\test1.sqlite
+
+sqlite3 Datasets/test1.sqlite
+
+
+-- after that following sql statements can be run
+
+.read src/main/resources/createFullKdd99Dataset.sql
