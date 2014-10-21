e1ab297f659522b81f8b3a9b2c81e6a91dbc2968
ati-ozgur <ati.ozgur@gmail.com>
2014-10-21 16:33:28 +0300

    script changes

--- src/main/resources/controlTables.sql
+++ src/main/resources/controlTables.sql
@@ -1,2 +1,4 @@
 select count(*) from TEST_CORRECTED;
 select count(*) from KDD_TRAIN_DATA;
+select count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_binary;
+select count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_five;
