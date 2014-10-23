f371cf175c063ad460a6b94958580f2f1a18a431
ati-ozgur <ati.ozgur@gmail.com>
2014-10-23 12:16:20 +0300

    More info about what task is doing.

--- src/main/groovy/testBench/ids/kdd99/TableSchemaUpdatesAfterImport.groovy
+++ src/main/groovy/testBench/ids/kdd99/TableSchemaUpdatesAfterImport.groovy
@@ -12,10 +12,14 @@ String sqlToBeExecuted = new File(Resources.SQL_SchemaChangeAfterImport).text
 // sqlEngine.execute.sqlToBeExecuted
 String[] sqlToBeExecutedList = sqlToBeExecuted.split(";")
 
+
+println "Starting to execute SQL Schema Update Commands"
+
 for(String sql in sqlToBeExecutedList) {
     boolean containsData = sql?.trim()
     if (containsData)
     {
+        println "Following SQL Command will be executed"
         println sql
         sqlEngine.execute sql
         println "--------------------- Table Schema Update Sql Executed ---------------------"
