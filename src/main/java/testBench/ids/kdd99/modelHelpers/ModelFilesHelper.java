18023a0225dc8c82af8fe91ca247d28f6b46e183
Atilla Ozgur <ati.ozgur@gmail.com>
2014-10-26 22:29:37 +0200

    ModelFilesHelper added

---
+++ src/main/java/testBench/ids/kdd99/modelHelpers/ModelFilesHelper.java
@@ -0,0 +1,71 @@
+package testBench.ids.kdd99.modelHelpers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import testBench.ids.kdd99.Finals;
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.Utils;
+
+public class ModelFilesHelper {
+	public static List<String>  getModelFileNames() {
+		final File folder = new File(Finals.MODELS_SAVE_FOLDER);
+		return  ModelFilesHelper.getModelFileNames(folder);
+	}
+	
+	public static List<String>  getModelFileNames(final File folder) {
+		
+		List<String> listOfFileNames = new ArrayList<String>();
+		
+		String fileName;
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	        	continue;
+	        } else {
+	        	fileName = fileEntry.getName();
+	        	if (fileName.endsWith(".model"))
+	        	{
+	        		
+		        	listOfFileNames.add(fileName);
+	        	}
+	        }
+	    }
+	    return listOfFileNames;
+	}
+	
+	
+	
+	public static void saveModels(Instances data, String pModelName
+			,String classifierFullName, String optionString) {
+		try {
+			AbstractClassifier classifier = (AbstractClassifier) Class.forName(classifierFullName).newInstance();
+			saveModels(data, pModelName, classifier, optionString);
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+			
+		}
+	}
+	
+	public static void saveModels(Instances data, String pModelName
+			,AbstractClassifier classifier, String optionString) {
+		try {
+			classifier.setOptions(Utils.splitOptions(optionString));
+		classifier.buildClassifier(data); // build classifier
+
+		String modelFullFileName = Finals.MODELS_SAVE_FOLDER + classifier.getClass().getName()
+				+ pModelName  + ".model";
+		SerializationHelper.write(modelFullFileName, classifier);
+		System.out.println("Model ( " +modelFullFileName + ")Saved");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+			
+		}
+	}
+
+
+	
+}
