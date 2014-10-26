00abd6172ef633b71cfc64bcbc2ed5b32cde2b31
Atilla Ozgur <ati.ozgur@gmail.com>
2014-10-26 20:32:30 +0200

    Update for TEST_CORRECTED

--- src/main/resources/SchemaChangeAfterImport.sql
+++ src/main/resources/SchemaChangeAfterImport.sql
@@ -3,3 +3,10 @@ update KDD_TRAIN_DATA SET class_attribute = 'normal' where class_attribute = 'no
 ALTER TABLE KDD_TRAIN_DATA ADD COLUMN class_attribute_binary text default 'attack';
 
 update KDD_TRAIN_DATA SET class_attribute_binary = 'normal' where class_attribute = 'normal';
+
+
+update TEST_CORRECTED SET class_attribute = 'normal' where class_attribute = 'normal.';
+
+ALTER TABLE TEST_CORRECTED ADD COLUMN class_attribute_binary text default 'attack';
+
+update TEST_CORRECTED SET class_attribute_binary = 'normal' where class_attribute = 'normal';
