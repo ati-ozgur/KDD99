c509435f08cb5d651d4854136886770ca1f550e5
Atilla Ozgur <ati.ozgur@gmail.com>
2014-10-26 21:56:40 +0200

    sql fix

--- src/main/resources/controlTables.sql
+++ src/main/resources/controlTables.sql
@@ -4,5 +4,5 @@ select class_attribute_five,count(*) from KDD_TRAIN_DATA GROUP BY class_attribut
 
 
 select count(*) from TEST_CORRECTED;
-select class_attribute_binary,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_binary;
-select class_attribute_five,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_five;
+select class_attribute_binary,count(*) from TEST_CORRECTED GROUP BY class_attribute_binary;
+select class_attribute_five,count(*) from TEST_CORRECTED GROUP BY class_attribute_five;
