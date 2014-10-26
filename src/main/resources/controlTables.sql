4ce20868c31e8d1e0c9ab9cc0ac60ce41409a950
Atilla Ozgur <ati.ozgur@gmail.com>
2014-10-26 21:45:19 +0200

    new control statements

--- src/main/resources/controlTables.sql
+++ src/main/resources/controlTables.sql
@@ -1,4 +1,8 @@
-select count(*) from TEST_CORRECTED;
 select count(*) from KDD_TRAIN_DATA;
+select class_attribute_binary,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_binary;
+select class_attribute_five,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_five;
+
+
+select count(*) from TEST_CORRECTED;
 select class_attribute_binary,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_binary;
 select class_attribute_five,count(*) from KDD_TRAIN_DATA GROUP BY class_attribute_five;
