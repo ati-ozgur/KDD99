be0e148ce132aad41447515d438f785656e943da
ati-ozgur <ati.ozgur@gmail.com>
2014-11-11 16:43:28 +0200

    memory output change

--- src/main/groovy/testBench/ids/kdd99/TrainModelsUsingDifferentClassifiersInTrainingDataSet.groovy
+++ src/main/groovy/testBench/ids/kdd99/TrainModelsUsingDifferentClassifiersInTrainingDataSet.groovy
@@ -15,6 +15,9 @@ import testBench.ids.kdd99.modelHelpers.*;
 Instances data = null;
 
 new File("Datasets/SavedOutputs").mkdir();
+int kb = 1024;
+int mb = kb*1024;
+int gb = mb*1024;
 
 ClassifiersAndArguments.classifierList.each() {
 	classifierAndOption-> 
@@ -31,10 +34,26 @@ ClassifiersAndArguments.classifierList.each() {
 	oneTrainRun.append("---------------------------------------------------------------------------------------Start\n");
 	oneTrainRun.append(String.format("Training Start at %s for dataset %s\n", getNowAsFormatted(), datasetName)) ;
 	oneTrainRun.append(String.format("Training Start at %s for model %s\n", getNowAsFormatted(), classifierName)) ;
+
 	ModelFilesHelper.saveModels(data,datasetName,classifierName,options);
-	oneTrainRun.append(String.format("freeMemory %s \n" , Runtime.getRuntime().freeMemory()));
-	oneTrainRun.append(String.format("totalMemory %s \n" , Runtime.getRuntime().totalMemory()));
-	oneTrainRun.append(String.format("maxMemory %s \n " , Runtime.getRuntime().maxMemory()));
+
+	int freeMemory = Runtime.getRuntime().freeMemory();
+	int totalMemory = Runtime.getRuntime().totalMemory();
+	int maxMemory = Runtime.getRuntime().maxMemory();
+	oneTrainRun.append(String.format("freeMemory %s as bytes \n" , freeMemory));
+	oneTrainRun.append(String.format("freeMemory %s as kilo bytes \n" , freeMemory/kb));
+	oneTrainRun.append(String.format("freeMemory %s as mega bytes \n" , freeMemory/mb));
+	oneTrainRun.append(String.format("freeMemory %s as giga bytes \n" , freeMemory/gb));
+
+	oneTrainRun.append(String.format("totalMemory %s as bytes \n" , totalMemory));
+	oneTrainRun.append(String.format("totalMemory %s as kilo bytes \n" , totalMemory/kb));
+	oneTrainRun.append(String.format("totalMemory %s as mega bytes \n" , totalMemory/mb));
+	oneTrainRun.append(String.format("totalMemory %s as giga bytes \n" , totalMemory/gb));
+
+	oneTrainRun.append(String.format("maxMemory %s as bytes \n" , maxMemory));
+	oneTrainRun.append(String.format("maxMemory %s as kilo bytes \n" , maxMemory/kb));
+	oneTrainRun.append(String.format("maxMemory %s as mega bytes \n" , maxMemory/mb));
+	oneTrainRun.append(String.format("maxMemory %s as giga bytes \n" , maxMemory/gb));
 	
 	oneTrainRun.append(String.format("Training Finish at %s for dataset %s \n", getNowAsFormatted(), datasetName) );
 	oneTrainRun.append(String.format("Training Finish at %s for Model %s  \n", getNowAsFormatted(), classifierName) );
