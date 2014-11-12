2ef9f626c7e2c822ea83984fa158d92d4a2d9acd
ati-ozgur <ati.ozgur@gmail.com>
2014-11-12 15:18:51 +0200

    refactor

--- src/main/groovy/testBench/ids/kdd99/ProgramGroovy.groovy
+++ src/main/groovy/testBench/ids/kdd99/ProgramGroovy.groovy
@@ -9,23 +9,23 @@ import java.util.Date;
 // weka.core.SystemInfo.main(null);
 
 
-Date d1 = DateHelper.getNow();
 
 
-final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
+def exampleMlTrainResults()
+{
+    Date d1 = DateHelper.getNow();
+    final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
+    long t=d1.getTime();
+    Date d2=new Date(t + (10 * ONE_MINUTE_IN_MILLIS));
 
-long t=d1.getTime();
-Date d2=new Date(t + (10 * ONE_MINUTE_IN_MILLIS));
-
-
-RuntimeInformation inf = RuntimeInformationHelper.getRuntimeInformation();
-
-MlTrainResultsDal.Ekle(
-    "combined_createFullKdd99TrainingDatasetForBinary"
-    ,"weka.classifiers.bayes.NaiveBayes"
-    ,""
-    ,d1
-    ,d2
-    ,inf
-    )
+    RuntimeInformation inf = RuntimeInformationHelper.getRuntimeInformation();
+    MlTrainResultsDal.Ekle(
+        "combined_createFullKdd99TrainingDatasetForBinary"
+        ,"weka.classifiers.bayes.NaiveBayes"
+        ,""
+        ,d1
+        ,d2
+        ,inf
+        )
+}
 
