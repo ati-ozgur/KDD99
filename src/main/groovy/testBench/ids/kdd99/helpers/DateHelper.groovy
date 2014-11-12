f4cc9ef35a0ab644129aabba8d33dc85ac51d31d
ati-ozgur <ati.ozgur@gmail.com>
2014-11-12 10:18:59 +0200

    refactor

--- src/main/groovy/testBench/ids/kdd99/helpers/DateHelper.groovy
+++ src/main/groovy/testBench/ids/kdd99/helpers/DateHelper.groovy
@@ -5,13 +5,21 @@ import java.util.Date;
 public class DateHelper
 {
 
+    public static final String STR_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     public static String getNowAsFormatted() {
         Calendar c = Calendar.getInstance();
         Date d = c.getTime();
         
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat(STR_DATE_FORMAT);
         return sdf.format(d);
     
     }
+    public static Date getNowAsFormatted() {
+        Calendar c = Calendar.getInstance();
+        Date d = c.getTime();
+        return d;
+    }
+
 
 }
