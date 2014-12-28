1899c0f90344bd733ebb2fe0bdcc8c88d81ab104
Atilla Ozgur <ati.ozgur@gmail.com>
2014-12-29 00:17:28 +0200

    fix

--- startFresh.bash
+++ startFresh.bash
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 export PATH=$PATH:`pwd`/tools
-=======
->>>>>>> FETCH_HEAD
 ./gradlew clean downloadKddDatasetFiles unzipKddDatasetFiles tableImportKddTrainDataFromCommandLine execSQLSchemaChangeAfterImport execCreateArffFileFromDatabase execTrainModelsUsingDifferentClassifiersInTrainingDataSet
 
