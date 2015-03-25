c8f736a258cb5ac7232eb14dceee7c6a20cca3a4
Atilla Ozgur <ati.ozgur@gmail.com>
2015-03-25 20:19:12 +0200

    train model code v2

--- src/main/groovy/testBench/ids/kdd99/TrainModelInTrainingDataSet.groovy
+++ src/main/groovy/testBench/ids/kdd99/TrainModelInTrainingDataSet.groovy
@@ -15,7 +15,25 @@ def map = [ "J48": "weka.classifiers.trees.J48",
 "BayesNet":"weka.classifiers.bayes.BayesNet",
 "NaiveBayes":"weka.classifiers.bayes.NaiveBayes",
 "Logistic": "weka.classifiers.functions.Logistic",
-"MultilayerPerceptron":"weka.classifiers.functions.MultilayerPerceptron"
+"MultilayerPerceptron":"weka.classifiers.functions.MultilayerPerceptron",
+"MLP":"weka.classifiers.functions.MultilayerPerceptron",
+"RBFNetwork": "weka.classifiers.functions.RBFNetwork",
+"RBFClassifier":"weka.classifiers.functions.RBFClassifier",
+"SGD":"weka.classifiers.functions.SGD",
+"SimpleLogistic":"weka.classifiers.functions.SimpleLogistic",
+"AdaBoostM1":"weka.classifiers.meta.AdaBoostM1",
+"DecisionStump":"weka.classifiers.trees.DecisionStump",
+"DecisionTable":"weka.classifiers.rules.DecisionTable",
+"OneR":"weka.classifiers.rules.OneR",
+"ZeroR":"weka.classifiers.rules.ZeroR",
+"JRip":"weka.classifiers.rules.JRip",
+"PART":"weka.classifiers.rules.PART",
+"RandomForest":"weka.classifiers.trees.RandomForest",
+"LMT":"weka.classifiers.trees.LMT",
+"RandomTree":"weka.classifiers.trees.RandomTree",
+"REPTree":"weka.classifiers.trees.REPTree",
+"IBk":"weka.classifiers.lazy.IBk"
+
 ]
 
 classifierName = args[1]
