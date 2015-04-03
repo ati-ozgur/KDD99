8d42783acd6250409a8b54ebc5837844564a2f76
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-03 10:19:58 +0300

    train using moa v1

---
+++ src/main/groovy/testBench/ids/kdd99/TrainOneModelUsingMOAInTrainingDataSet.groovy
@@ -0,0 +1,55 @@
+package testBench.ids.kdd99;
+
+
+import moa.classifiers.*;
+import moa.classifiers.trees.HoeffdingTree;
+
+import weka.core.Instance;
+import weka.core.Instances;
+
+
+import moa.streams.generators.*;
+import moa.streams.*;
+
+int numInstances=5 * 1000 * 1000;
+
+
+
+String datasetName = "combined_createFullKdd99TrainingDatasetForBinary";
+String datasetNameFullFileName = Finals.ARFF_SAVE_FOLDER + datasetName + ".arff";
+
+println(datasetNameFullFileName);
+
+
+
+Classifier learner = new HoeffdingTree ( ) ;
+
+int classIndex = 42;
+ArffFileStream stream = new ArffFileStream(datasetNameFullFileName, classIndex)
+
+stream.prepareForUse();
+
+ learner.setModelContext(stream.getHeader()) ;
+ learner.prepareForUse() ;
+
+ int numberSamplesCorrect=0;
+ int numberSamples=0;
+ boolean isTesting = true ;
+ while( stream.hasMoreInstances () && numberSamples < numInstances){
+     Instance trainInst = stream.nextInstance() ;
+     if ( isTesting ){
+         if ( learner.correctlyClassifies(trainInst )){
+             numberSamplesCorrect++;
+         }
+     }
+     numberSamples++;
+     learner.trainOnInstance(trainInst) ;
+ }
+
+
+
+ double accuracy = 100.0*(double) numberSamplesCorrect / (double) numberSamples ;
+ 
+ println (numberSamples+ " instances processed with " + accuracy+ "% accuracy" )
+
+
