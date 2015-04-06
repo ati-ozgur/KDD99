4751d8d58381d5dd5452a4e75f07886e029c2985
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-06 17:13:10 +0300

    ClassifiersAndArgumentsMoa added

--- src/main/groovy/testBench/ids/kdd99/moa/ClassifiersAndArgumentsMoa.groovy
+++ src/main/groovy/testBench/ids/kdd99/moa/ClassifiersAndArgumentsMoa.groovy
@@ -1,4 +1,4 @@
-package testBench.ids.kdd99;
+package testBench.ids.kdd99.moa;
 
 public class ClassifiersAndArgumentsMoa
 {
@@ -7,6 +7,35 @@ public class ClassifiersAndArgumentsMoa
     private static final template = [ Classifier:"",Options:""]
 
 
+
+    private static final NaiveBayes = [ Classifier:"moa.classifiers.bayes.NaiveBayes",Options:""]
+    private static final Perceptron = [ Classifier:"moa.classifiers.functions.Perceptron",Options:""]
+    private static final SGD = [ Classifier:"moa.classifiers.functions.SGD",Options:""]
+    private static final SPegasos = [ Classifier:"moa.classifiers.functions.SPegasos",Options:""]
+    private static final kNN = [ Classifier:"moa.classifiers.lazy.kNN",Options:""]
+    private static final OCBoost = [ Classifier:"moa.classifiers.meta.OCBoost",Options:""]
+    private static final OzaBag = [ Classifier:"moa.classifiers.meta.OzaBag",Options:""]
+    private static final OzaBoost = [ Classifier:"moa.classifiers.meta.OzaBoost",Options:""]
+    private static final RuleClassifier = [ Classifier:"moa.classifiers.rules.RuleClassifier",Options:""]
+    private static final RuleClassifierNBayes = [ Classifier:"moa.classifiers.rules.RuleClassifierNBayes",Options:""]
+    private static final DecisionStump = [ Classifier:"moa.classifiers.trees.DecisionStump",Options:""]
+    private static final HoeffdingTree = [ Classifier:"moa.classifiers.trees.HoeffdingTree",Options:""]
+
+
+
+
+    public static classifierListFull = [
+    NaiveBayes ,
+    Perceptron ,
+    SGD ,
+    kNN ,
+    OzaBag ,
+    RuleClassifier ,
+    RuleClassifierNBayes ,
+    DecisionStump  ,
+	HoeffdingTree      
+    
+    ] 
 
     public static classifierList = classifierListFull
 
