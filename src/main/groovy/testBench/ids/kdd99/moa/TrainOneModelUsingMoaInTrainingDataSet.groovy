d06a5a34ce3feca72cd1b4a508d546d87e367575
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-06 16:11:18 +0300

    small change

--- src/main/groovy/testBench/ids/kdd99/moa/TrainOneModelUsingMoaInTrainingDataSet.groovy
+++ src/main/groovy/testBench/ids/kdd99/moa/TrainOneModelUsingMoaInTrainingDataSet.groovy
@@ -4,7 +4,9 @@ package testBench.ids.kdd99.moa;
 
 import testBench.ids.kdd99.*;
 
+import moa.options.*;
 import weka.core.*;
+import moa.tasks.*;
 
 import moa.classifiers.*;
 import moa.classifiers.trees.HoeffdingTree;
@@ -14,42 +16,68 @@ import moa.streams.*;
 int numInstances=5 * 1000 * 1000;
 
 
+String options = ""
+String classifierName = ""
+
+println args
+
+firstArg = args[0];
+
+if(firstArg == "classifierName")
+{
+    shortClassifierName = args[1];
+    if (shortClassifierName == null)
+    {
+        println "$shortClassifierName is not found"
+        return;
+    }
+    classifierName =  ClassifierNameHelper.getLongName(shortClassifierName)
+
+    if(args.length > 2 && args[2] == "classifierOptions")
+    {
+        options = args[3];
+    }
+}
+
+if(firstArg == "classifierFullString")
+{
+    String classifierFullString = args[1]
+    int firstSpace = classifierFullString.indexOf(" ");
+    classifierName = classifierFullString.substring(0,firstSpace);
+    options = classifierFullString.substring(firstSpace,classifierFullString.length())
+
+    println classifierName
+    println options
+}
+
+
 
 String datasetName = "combined_createFullKdd99TrainingDatasetForBinary";
-String datasetNameFullFileName = Finals.ARFF_SAVE_FOLDER + datasetName + ".arff";
+String datasetFullFileName = Finals.ARFF_SAVE_FOLDER + datasetName + ".arff";
 
-println(datasetNameFullFileName);
+println(datasetFullFileName);
+
+String modelName = classifierName
+String modelFullFileName = Finals.MODELS_SAVE_FOLDER + modelName + ".moa";
+println(modelFullFileName);
 
 
-
-Classifier classifer = new HoeffdingTree ( ) ;
-
-int classIndex = 42;
-ArffFileStream stream = new ArffFileStream(datasetNameFullFileName, classIndex)
-
-stream.prepareForUse();
-
- classifer.setModelContext(stream.getHeader()) ;
- classifer.prepareForUse() ;
-
- int numberSamplesCorrect=0;
- int numberSamples=0;
- boolean isTesting = true ;
- while( stream.hasMoreInstances () && numberSamples < numInstances){
-     Instance trainInst = stream.nextInstance() ;
-     if ( isTesting ){
-         if ( classifer.correctlyClassifies(trainInst )){
-             numberSamplesCorrect++;
-         }
-     }
-     numberSamples++;
-     classifer.trainOnInstance(trainInst) ;
- }
+                // create standard options
+FlagOption suppressStatusOutputOption = new FlagOption("suppressStatusOutput", 'S' as char,"Suppress the task status output that is normally send to stderr.");
+FlagOption suppressResultOutputOption = new FlagOption("suppressResultOutput", 'R' as char,"Suppress the task result output that is normally send to stdout.");
+IntOption statusUpdateFrequencyOption = new IntOption("statusUpdateFrequency",'F' as char, "How many milliseconds to wait between status updates.", 1000, 0, Integer.MAX_VALUE);
 
 
 
- double accuracy = 100.0*(double) numberSamplesCorrect / (double) numberSamples ;
- 
- println (numberSamples+ " instances processed with " + accuracy+ "% accuracy" )
+moa.options.Option[] extraOptions = [ suppressStatusOutputOption, suppressResultOutputOption, statusUpdateFrequencyOption];
 
+String cliString = "LearnModel -l $classifierName $options -s (ArffFileStream -f $datasetFullFileName -c -1) -m 5000000  -O $modelFullFileName"
 
+println(cliString)
+
+// parse options
+Task task = (Task) ClassOption.cliStringToObject(cliString, Task.class, extraOptions);
+Object result =  task.doTask();;
+
+println(result)
+println(result.getClass())
