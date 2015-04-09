f171fafdd0625db221cbe19980b711a313be6bc7
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-09 20:48:10 +0300

    R Constant added

---
+++ src/main/R/Constants.R
@@ -0,0 +1,4 @@
+FOLDER_MAIN <- "../../../"
+FOLDER_DATASET <- paste (FOLDER_MAIN,"Datasets", sep = "" )
+FOLDER_RESOURCES <- paste (FOLDER_MAIN,"src/main/resources/", sep = "" )
+FOLDER_LATEX_TABLE_SQL <- paste(FOLDER_RESOURCES,"latexTableSql",sep = "")
