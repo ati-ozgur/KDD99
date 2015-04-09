57175c5b1ff380ff6772726f04d699b88015421d
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-09 20:59:39 +0300

    sqlite to latex for hardware metrics

---
+++ src/main/R/SqliteToLatexClassifierTrainingHardwareMetrics.R
@@ -0,0 +1,13 @@
+source("Constants.R")
+
+sqlText <- singleString <- paste(readLines(ClassifierTrainingHardwareMetricsFileName), collapse="\n")
+
+
+dbIDS <- src_sqlite(DATASET_FILENAME, create = F)
+
+tbl.ClassifierTrainingHardwareMetricsFileName <- tbl(dbIDS, sql(sqlText))
+df.ClassifierTrainingHardwareMetricsFileName  <- as.data.frame(tbl.ClassifierTrainingHardwareMetricsFileName , n=-1)
+
+
+library(stargazer)
+stargazer(df.ClassifierTrainingHardwareMetricsFileName, summary=FALSE)
