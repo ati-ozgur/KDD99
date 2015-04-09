cf95d3aa008dca65e9c5bc5202b23bb958a1d841
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-09 13:52:25 +0300

    TestAllModels v1

--- src/main/groovy/testBench/ids/kdd99/moa/TestAllModelsUsingMoaInTrainingDataSet.groovy
+++ src/main/groovy/testBench/ids/kdd99/moa/TestAllModelsUsingMoaInTrainingDataSet.groovy
@@ -9,8 +9,20 @@ import testBench.ids.kdd99.datasetHelpers.*;
 import testBench.ids.kdd99.modelHelpers.*;
 import testBench.ids.kdd99.helpers.*;
 
+import groovy.io.FileType;
+
 
 def dir = new File(Finals.MODELS_SAVE_FOLDER )
-dir.eachFileRecurse (FileType.FILES) { file ->
-    println(file)
+dir.eachFile (FileType.FILES) { file ->
+     if(file.name.endsWith('.moa')) {
+        println(file.name)
+        
+
+        String[] argList = new String[2];
+        argList[0] = "modelFileName";
+        argList[1] = file.name;
+
+        TestOneModelUsingMoaInTestDataSet.main(argList)        
+    }
+
 }
