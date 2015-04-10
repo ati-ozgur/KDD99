829f5bf70760bf5b80779418318566f1a46b7e49
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-10 10:39:05 +0300

    sql change

--- src/main/resources/latexTableSql/ClassifierTrainingInformation.sql
+++ src/main/resources/latexTableSql/ClassifierTrainingInformation.sql
@@ -1,10 +1,14 @@
-SELECT S.classifierName
-,AVG(S.trainingDurationMinutes) AS trainingDurationMinutes
+SELECT 
+CN.ClassifierShortName
+, ROUND(AVG(S.trainingDurationSecond),3) || 'm' AS trainingDurationSecond
+, ROUND(AVG(S.trainingDurationMinutes),3) || 'm' AS trainingDurationMinutes
 ,AVG(S.usedMemoryMb) AS usedMemoryMb
 ,AVG(S.usedMemoryGb) AS usedMemoryGb
 ,MAX(S.ModelSize) ModelSize
 ,MAX(S.modelSizeMb) ModelSizeMb
 
 FROM ML_TRAIN_RESULTS_SUMMARY S
+LEFT JOIN LT_CLASSIFIER_NAMES CN
+ON CN.ClassifierFullName = S.classifierName
 WHERE S.classifierName LIKE 'weka%'
 GROUP BY S.classifierName
