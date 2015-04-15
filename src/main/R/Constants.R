08ccb164d4c3c2f72b76a728582e2578743b48be
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-15 22:19:50 +0300

    sql filename fix

--- src/main/R/Constants.R
+++ src/main/R/Constants.R
@@ -1,6 +1,6 @@
 FOLDER_MAIN <- "../../../"
 FOLDER_DATASET <- paste (FOLDER_MAIN,"Datasets/", sep = "" )
-FOLDER_RESOURCES <- paste (FOLDER_MAIN,"src/main/resources/", sep = "" )
+FOLDER_RESOURCES <- paste (FOLDER_MAIN,"src/main/SQL/", sep = "" )
 FOLDER_LATEX_TABLE_SQL <- paste(FOLDER_RESOURCES,"latexTableSql/",sep = "")
 
 DATASET_FILENAME <- paste(FOLDER_DATASET,"test1.sqlite",sep = "")
