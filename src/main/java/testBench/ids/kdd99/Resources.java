91cb70600cb2ee23edf8093eda28c2147197ff5a
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-15 17:36:34 +0300

    sql filenames changed

--- src/main/java/testBench/ids/kdd99/Resources.java
+++ src/main/java/testBench/ids/kdd99/Resources.java
@@ -3,13 +3,17 @@ package testBench.ids.kdd99;
 public class Resources
 {
 
-    public static final String SQL_MAIN_FOLDER=Finals.RESOURCES;
-    public static final String SQL_kdd99TableCreation = Finals.RESOURCES + "kdd99TableCreation.sql";
-    public static final String SQL_kdd99LtTableInserts = Finals.RESOURCES + "kdd99LtTableInserts.sql";
-    public static final String SQL_SchemaChangeAfterImport = Finals.RESOURCES + "SchemaChangeAfterImport.sql";
+  
+    public static final String SQL_MAIN_FOLDER = "src/main/SQL/";
+
+    public static final String SQL_KDD99ETLScripts = SQL_MAIN_FOLDER + "KDD99ETLScripts/";
+
+    public static final String SQL_kdd99TableCreation = SQL_KDD99ETLScripts + "kdd99TableCreation.sql";
+    public static final String SQL_kdd99LtTableInserts = SQL_KDD99ETLScripts + "kdd99LtTableInserts.sql";
+    public static final String SQL_SchemaChangeAfterImport = SQL_KDD99ETLScripts + "SchemaChangeAfterImport.sql";
 
     
-    public static final String SQL_Import = Finals.RESOURCES + "import.sql";
+    public static final String SQL_Import = SQL_KDD99ETLScripts + "importDataToSqlite.sql";
 
 
 
