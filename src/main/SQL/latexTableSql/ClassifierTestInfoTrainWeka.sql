e9fb02f2f28c357af23a4306ed94d462fb963231
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-16 22:35:58 +0300

    new sql test on training

---
+++ src/main/SQL/latexTableSql/ClassifierTestInfoTrainWeka.sql
@@ -0,0 +1,19 @@
+SELECT 
+COALESCE(CN.ClassifierShortName,T.classifierName) AS "Classifier Name"
+, max(T.accuracy) AS Accuracy
+, max(T.tprate) AS "True Positive Rate"
+, max(T.tnrate) AS "True Negative Rate"
+, max(T.fprate) AS "False Negative Rate"
+, max(T.fnrate) AS "False Negative Rate"
+, max(T.F1_score)
+
+FROM ML_TEST_RESULTS_SUMMARY T
+LEFT JOIN LT_CLASSIFIER_NAMES CN
+ON CN.ClassifierFullName = T.classifierName
+
+WHERE datasetType = 'Train'
+AND
+T.classifierName LIKE 'weka%'
+GROUP BY T.classifierName
+;
+
