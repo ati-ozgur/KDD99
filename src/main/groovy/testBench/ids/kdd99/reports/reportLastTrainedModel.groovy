c3f64fe58fd9599ee05dc43daaa7245a401a8137
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-17 16:50:03 +0300

    first report task

---
+++ src/main/groovy/testBench/ids/kdd99/reports/reportLastTrainedModel.groovy
@@ -0,0 +1,15 @@
+package testBench.ids.kdd99.reports;
+
+import groovy.sql.Sql
+
+
+
+def sqlEngine = Sql.newInstance(Finals.JDBC_URL)
+
+
+
+def sql = "SELECT * FROM ML_TRAIN_RESULTS";
+
+sqlEngine.eachRow(sql) { row ->
+     println row
+ }
