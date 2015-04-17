6cd48cde10092b40890fc596dd7069db9e55c4b9
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-17 18:54:37 +0300

    data set names change

--- src/main/java/testBench/ids/kdd99/DataSetFiles.java
+++ src/main/java/testBench/ids/kdd99/DataSetFiles.java
@@ -6,12 +6,16 @@ public class DataSetFiles
     public static final String KddcupExampleData = Finals.DATASET_FILES + "KddcupExampleData";
     public static final String KddcupTrainData = Finals.DATASET_FILES + "kddcup.data";
 
-    public static final String KddcupTrainingDatasetForBinaryArff = Finals.DATASET_FILES + "SavedArff/combined_createFullKdd99TrainingDatasetForBinary-4898431.arff";
-    public static final String KddcupTestDatasetForBinaryArff = Finals.DATASET_FILES + "SavedArff/combined_createFullKdd99TestDatasetForBinary-311029.arff";
+    private static final String _TrainDatasetName = "Kdd99TrainingDatasetForBinary"
+    private static final String _TestDatasetName = "Kdd99TestDatasetForBinary"
 
 
-    public static final String KddcupTrainingDatasetForBinaryFullTemplateArff = Finals.DATASET_FILES + "SavedArff/Kdd99TrainingDatasetForBinary.arff";
-    public static final String KddcupTestDatasetForBinaryFullTemplateArff = Finals.DATASET_FILES + "SavedArff/combined_createFullKdd99TestDatasetForBinary.arff";
+    public static final String KddcupTrainingDatasetForBinaryArff = Finals.DATASET_FILES + "SavedArff/" + _TrainDatasetName+ "-4898431.arff";
+    public static final String KddcupTestDatasetForBinaryArff = Finals.DATASET_FILES + "SavedArff/"+ _TestDatasetName +"-311029.arff";
+
+
+    public static final String KddcupTrainingDatasetForBinaryFullTemplateArff = Finals.DATASET_FILES + "SavedArff/"+ _TrainDatasetName + ".arff";
+    public static final String KddcupTestDatasetForBinaryFullTemplateArff = Finals.DATASET_FILES + "SavedArff/" + _TestDatasetName + ".arff";
 
 
 
