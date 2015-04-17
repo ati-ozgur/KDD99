745cf74b805c03355425a3f1d114d3d702fa9599
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-17 17:32:27 +0300

    report v1

--- src/main/groovy/testBench/ids/kdd99/reports/reportLastTrainedModel.groovy
+++ src/main/groovy/testBench/ids/kdd99/reports/reportLastTrainedModel.groovy
@@ -3,8 +3,8 @@ package testBench.ids.kdd99.reports;
 
 import testBench.ids.kdd99.*;
 
-import groovy.sql.Sql
-
+import groovy.sql.*;
+import java.sql.*;
 
 
 def sqlEngine = Sql.newInstance(Finals.JDBC_URL)
@@ -13,6 +13,15 @@ def sqlEngine = Sql.newInstance(Finals.JDBC_URL)
 
 def sql = "SELECT * FROM ML_TRAIN_RESULTS";
 
+
+
 sqlEngine.eachRow(sql) { row ->
-     println row
- }
+    println row
+    int columnCount = row.getMetaData().columnCount;
+    //println columnCount
+
+    (0..columnCount-1).each{ i ->
+        println row.getMetaData().getColumnName(i+1) + ":" + row[i]
+    }
+}
+ 
