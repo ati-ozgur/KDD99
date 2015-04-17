901da2ee61ac8b4a4a9c4e7db790ec231314dc9e
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-17 19:00:44 +0300

    compile error

--- src/main/java/testBench/ids/kdd99/DataSetFiles.java
+++ src/main/java/testBench/ids/kdd99/DataSetFiles.java
@@ -6,8 +6,8 @@ public class DataSetFiles
     public static final String KddcupExampleData = Finals.DATASET_FILES + "KddcupExampleData";
     public static final String KddcupTrainData = Finals.DATASET_FILES + "kddcup.data";
 
-    private static final String _TrainDatasetName = "Kdd99TrainingDatasetForBinary"
-    private static final String _TestDatasetName = "Kdd99TestDatasetForBinary"
+    private static final String _TrainDatasetName = "Kdd99TrainingDatasetForBinary";
+    private static final String _TestDatasetName = "Kdd99TestDatasetForBinary";
 
 
     public static final String KddcupTrainingDatasetForBinaryArff = Finals.DATASET_FILES + "SavedArff/" + _TrainDatasetName+ "-4898431.arff";
