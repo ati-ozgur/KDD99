8f340883c5152d49ce6a673b6c748e4a65b83f1f
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-30 18:37:51 +0300

    new report task added

---
+++ src/main/groovy/testBench/ids/kdd99/reports/reportTestShowLast.groovy
@@ -0,0 +1,27 @@
+package testBench.ids.kdd99.reports;
+
+
+import testBench.ids.kdd99.*;
+
+import groovy.sql.*;
+import java.sql.*;
+
+
+def sqlEngine = Sql.newInstance(Finals.JDBC_URL)
+
+
+
+def sql = "SELECT * FROM ML_TEST_RESULTS ORDER BY ID DESC LIMIT 1";
+
+
+
+sqlEngine.eachRow(sql) { row ->
+    //println row
+    int columnCount = row.getMetaData().columnCount;
+    //println columnCount
+
+    (0..columnCount-1).each{ i ->
+        println row.getMetaData().getColumnName(i+1) + ":" + row[i]
+    }
+}
+ 
