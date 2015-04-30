48568435c98b3e1b2c4ceb99446ba9231633c29d
Atilla Ozgur <ati.ozgur@gmail.com>
2015-04-30 18:27:52 +0300

    more information on testing stage

--- src/main/groovy/testBench/ids/kdd99/weka/TestAllModelsUsingWekaInTestDataSet.groovy
+++ src/main/groovy/testBench/ids/kdd99/weka/TestAllModelsUsingWekaInTestDataSet.groovy
@@ -18,14 +18,20 @@ import weka.core.Instances;
 
 List<String> modelFileNames = ModelFilesHelper.getModelFileNamesWeka()
 
-Instances trainDataset = DataSetHelper.getTrainDatasetFull();
+println("All Trained Model Count: " + modelFileNames.size());
 
-for(String modelFileName: modelFileNames)
+println(modelFileNames)
+
+//Instances trainDataset = DataSetHelper.getTrainDatasetFull();
+
+for(int i=0;i< modelFileNames.size();i++)
 {
-
+    String modelFileName = modelFileNames.get(i);
     String[] argList = new String[2];
     argList[0] = "modelFileName";
     argList[1] = modelFileName;
+
+    println("Testing Model number $i and name $modelFileName in full Training Dataset ")
 
     TestOneModelUsingWekaInTestDataSet.main(argList)
 
