6b4f6f6e09acaa363f237de03f4dd8a3d28f4d32
Atilla Ozgur <ati.ozgur@gmail.com>
2015-05-08 16:25:45 +0300

    test ok, random numbers are same

--- src/main/groovy/testBench/ids/kdd99/dal/CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy
+++ src/main/groovy/testBench/ids/kdd99/dal/CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy
@@ -11,4 +11,25 @@ import testBench.ids.kdd99.helpers.*;
 println "CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy"
 
 
-final int MAX_ROW = 10000000
+final int MAX_ROW = 10;//Finals.KDD_TRAIN_DATA_NODUP_COUNT / 2;
+
+
+
+int i=0;
+
+print("[")
+while(true)
+{
+    int rndNumber = RandomNumbersHelpers.randIntKddTrainDataNoDuplicate();
+    print("" + rndNumber + ",")
+    i++;
+
+    if(i > MAX_ROW)
+    {
+        break;
+    }
+
+
+}
+
+print("]")
