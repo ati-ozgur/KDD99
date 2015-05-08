58e209627695ca9bc05f78908798775de1d1634d
Atilla Ozgur <ati.ozgur@gmail.com>
2015-05-08 16:31:52 +0300

    with replacement seems ok

--- src/main/groovy/testBench/ids/kdd99/dal/CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy
+++ src/main/groovy/testBench/ids/kdd99/dal/CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy
@@ -1,7 +1,7 @@
 package testBench.ids.kdd99.dal;
 
 
-
+import groovy.sql.Sql
 import testBench.ids.kdd99.*;
 import testBench.ids.kdd99.helpers.*;
 
@@ -11,25 +11,26 @@ import testBench.ids.kdd99.helpers.*;
 println "CreateSubsetDataWithReplacementKdd99TrainDataNoDup.groovy"
 
 
-final int MAX_ROW = 10;//Finals.KDD_TRAIN_DATA_NODUP_COUNT / 2;
+final int MAX_ROW = Finals.KDD_TRAIN_DATA_NODUP_COUNT / 2;
 
+def sqlEngine = Sql.newInstance(Finals.JDBC_URL);
 
 
 int i=0;
 
-print("[")
+def SQL_INSERT = "INSERT INTO SUBSET_IDS_WITH_REPLACEMENT(val) VALUES(?)"
+
 while(true)
 {
     int rndNumber = RandomNumbersHelpers.randIntKddTrainDataNoDuplicate();
-    print("" + rndNumber + ",")
+    sqlEngine.execute(SQL_INSERT, rndNumber)
     i++;
-
     if(i > MAX_ROW)
     {
+
         break;
     }
 
 
 }
-
-print("]")
+sqlEngine.close()
