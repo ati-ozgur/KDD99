d2fe4f597259152216bbc98d71cbf5a82c8ae824
Atilla Ozgur <ati.ozgur@gmail.com>
2015-05-12 18:00:32 +0300

    sql fix

--- src/main/SQL/latexTableSql/ClassifierTrainingInformationHardwareMetricsWeka.sql
+++ src/main/SQL/latexTableSql/ClassifierTrainingInformationHardwareMetricsWeka.sql
@@ -2,7 +2,7 @@ SELECT
 COALESCE(CN.ClassifierShortName,S.classifierName) AS "Classifier Name"
 ,ROUND(AVG(S.trainingDurationSeconds)) || 's (' || ROUND(AVG(S.trainingDurationMinutes),3) || 'm)' AS "Training Duration"
 ,ROUND(AVG(S.usedMemoryMb)) || 'Mb (' || ROUND(AVG(S.usedMemoryGb),3) || 'Gb)' AS "Used Memory"
-,MAX(S.ModelSize)  || 'kb (' ||  MAX(S.modelSizeMb) || 'Gb)'AS  "Model Size"
+,ROUND(MAX(S.ModelSize/1024))  || 'kb (' ||  ROUND(MAX(S.modelSizeMb)) || 'Mb)'AS  "Model Size"
 FROM ML_TRAIN_RESULTS_SUMMARY S
 LEFT JOIN LT_CLASSIFIER_NAMES CN
 ON CN.ClassifierFullName = S.classifierName
