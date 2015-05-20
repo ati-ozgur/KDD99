f72acf2cc8fdf6ae131716073223a11dad5dce6d
Atilla Ozgur <ati.ozgur@gmail.com>
2015-05-20 16:43:25 +0300

    Learning Curve v2

--- src/main/groovy/testBench/ids/kdd99/dal/CreateArffFilesForLearningCurve.groovy
+++ src/main/groovy/testBench/ids/kdd99/dal/CreateArffFilesForLearningCurve.groovy
@@ -5,14 +5,35 @@ import testBench.ids.kdd99.helpers.modelHelpers.*;
 import testBench.ids.kdd99.helpers.datasetHelpers.*;
 
 
-int startSize = 1024;
-int finishSize = 1074993;
 
+final int MAX_ROW = Finals.KDD_TRAIN_DATA_COUNT;
+
+def listOfSize = [1024
+,2048
+,4096
+,8192
+,16384
+,32768
+,65536
+,131072
+,262144
+,524288
+,524288 * 2
+,524288 * 3
+,524288 * 4
+,524288 * 5
+,524288 * 6
+,524288 * 7
+,524288 * 8
+,524288 * 9
+,MAX_ROW
+
+]
 
 String templateName ="Kdd99TrainSubsetWithReplacement"; 
 
-for(int i=startSize; i<finishSize; i = i*2 )
-{
+listOfSize.each{ i -> 
+
     println(i);
     sqlFileName ="${templateName}Template.sql"
     String sql = new File(Resources.SQL_DatasetPreperationsScripts + sqlFileName).text
@@ -21,8 +42,8 @@ for(int i=startSize; i<finishSize; i = i*2 )
 
     String fileNameCombined = "${templateName}.arff"
     CreateArffFilesFromDatabase.createArffFileFromDatabaseQuery(sql,fileNameCombined)
-
 }
+
 
 
 
