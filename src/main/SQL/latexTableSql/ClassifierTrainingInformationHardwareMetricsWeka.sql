4f742f6b1212f7cc9cd5b557ce6e45be226fbcd9
Atilla Ozgur <ati.ozgur@gmail.com>
2015-05-21 10:58:32 +0300

    order by change

--- src/main/SQL/latexTableSql/ClassifierTrainingInformationHardwareMetricsWeka.sql
+++ src/main/SQL/latexTableSql/ClassifierTrainingInformationHardwareMetricsWeka.sql
@@ -1,6 +1,7 @@
 SELECT 
 COALESCE(CN.ClassifierShortName,S.classifierName) AS "Classifier Name"
 ,ROUND(AVG(S.trainingDurationSeconds)) || 's (' || ROUND(AVG(S.trainingDurationMinutes),3) || 'm)' AS "Training Duration"
+,ROUND(AVG(S.totalMemoryMb)) || 'Mb (' || ROUND(AVG(S.totalMemoryGb),3) || 'Gb)' AS "Used Memory"
 ,ROUND(AVG(S.usedMemoryMb)) || 'Mb (' || ROUND(AVG(S.usedMemoryGb),3) || 'Gb)' AS "Used Memory"
 ,ROUND(MAX(S.ModelSize/1024))  || 'kb (' ||  ROUND(MAX(S.modelSizeMb)) || 'Mb)'AS  "Model Size"
 FROM ML_TRAIN_RESULTS_SUMMARY S
@@ -8,4 +9,4 @@ LEFT JOIN LT_CLASSIFIER_NAMES CN
 ON CN.ClassifierFullName = S.classifierName
 WHERE S.classifierName LIKE 'weka%'
 GROUP BY S.classifierName
-ORDER BY CN.ClassifierFullName ASC
+ORDER BY CN.ClassifierShortName ASC
