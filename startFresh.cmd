815ce8cf9e8cfe2bafeb443ab861a81e2f5f411b
Atilla Ozgur <ati.ozgur@gmail.com>
2016-12-28 18:23:52 +0300

    start fresh script change

--- startFresh.cmd
+++ startFresh.cmd
@@ -1,6 +1,17 @@
 SET PATH=%PATH%;.\tools
-gradlew backupFull
+REM IF EXIST Datasets/ TYPE myfile.txt
+
+call gradlew SQLiteControlTablesExists
+if %ERRORLEVEL% == 0 goto :next
+echo "Errors encountered during execution.  Exited with status: %errorlevel%"
+goto :endofscript
+
+:next
+echo "Doing Backup"
+gradlew gradlew backupFull
+
+
+:endofscript
 gradlew clean
 START startFreshWithoutClean.cmd
-
-
+echo "Script complete"
